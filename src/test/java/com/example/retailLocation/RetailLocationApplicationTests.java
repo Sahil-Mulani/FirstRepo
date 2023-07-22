5b50e08d5fbc7b773db9a1fe4b9010e52070ada5
Sahil-Mulani <sahilmulani646@gmail.com>
2023-07-22 22:08:52 +0530

    this is first project of r service

---
+++ src/test/java/com/example/retailLocation/RetailLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retailLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
